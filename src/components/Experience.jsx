0fbbd5ed8cbffb38b06a9810bb0e48ea35adb018
yonnk <yonnk2019@163.com>
2024-07-14 17:53:43 +0800

    Ball & Tech

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -14,17 +14,33 @@ const ExperienceCard = ({ experience }) => (
     date={experience.date}
     iconStyle={{ background: experience.iconBg }}
     icon={
-      <div>
+      <div className='flex justify-center items-center w-full h-full'>
         <img
           alt={experience.company_name}
           src={experience.icon}
-          className="w-[100%] h-[60%] object-contain"
+          className="w-[60%] h-[60%] object-contain"
         />
       </div>
     }
   >
     <div>
       <h3 className="text-white text-[24px] font-bold">{experience.title}</h3>
+      <p
+        className="text-secondary text-[16px] font-semibold"
+        style={{margin: 0}}
+      >
+        {experience.company_name}
+      </p>
+      <ul>
+        {experience.points.map((point,index) => (
+          <li 
+            key={`experience-point-${index}`}
+            className="text-white-100 text-[14px] pl-1 tracking-wider"
+          >
+            {point}
+          </li>
+        ))}
+      </ul>
     </div>
   </VerticalTimelineElement>
 )
